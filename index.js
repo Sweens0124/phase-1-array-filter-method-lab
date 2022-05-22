bf8f34ef65594298d173dbc5cdbf9edd318421b6
Lucas Sweeney <lsweeney0124@gmail.com>
2022-05-21 20:01:56 -0600

    commit tree 9142207e29d9cfddef9785922fb2969bafe129f5

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching (drivers, name) {
+  const filter = drivers.filter(driver => driver.toLowerCase() === name.toLowerCase())
+  return filter
+}
+function fuzzyMatch (drivers, name) {
+  const filter = drivers.filter(driver => driver.toLowerCase().indexOf(name.toLowerCase()) === 0)
+  return filter
+}
+function matchName (drivers, name) {
+  const filter = drivers.filter(driver => driver.name === name)
+  return filter
+}
